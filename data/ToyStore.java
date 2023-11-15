731bd61796cc1a8d09faafcd694c8202fdf5fd98
PoladiyVictoria <vikapoladiy@gmail.com>
2023-11-15 22:14:17 +0300

    Init commit

---
+++ data/ToyStore.java
@@ -0,0 +1,16 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToyStore {
+    private List<Toy> toys;
+    private List<Toy> prizeToys;
+
+    public ToyStore() {
+        toys = new ArrayList<>();
+        prizeToys = new ArrayList<>();
+    }
+
+    
+}
